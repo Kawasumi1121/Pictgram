42c8c65acf3952d178f985a6329fa418e73e796e
kawasumi <wqgxy387@yahoo.co.jp>
2022-05-01 15:38:46 +0900

    PictgramApp #24

--- src/main/java/com/example/pictgram/PictgramApplication.java
+++ src/main/java/com/example/pictgram/PictgramApplication.java
@@ -1,10 +1,21 @@
 package com.example.pictgram;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PictgramApplication {
+	@Bean
+	public WebClient rest(ClientRegistrationRepository clients, OAuth2AuthorizedClientRepository authz) {
+		ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 = new ServletOAuth2AuthorizedClientExchangeFilterFunction(
+				clients, authz);
+		return WebClient.builder().filter(oauth2).build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PictgramApplication.class, args);
