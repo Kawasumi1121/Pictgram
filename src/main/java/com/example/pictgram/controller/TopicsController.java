358f8847a880d4baee94f1da2c8d117b41c23e4b
kawasumi <wqgxy387@yahoo.co.jp>
2022-05-01 14:52:15 +0900

    TopicsController #22

--- src/main/java/com/example/pictgram/controller/TopicsController.java
+++ src/main/java/com/example/pictgram/controller/TopicsController.java
@@ -56,7 +56,13 @@ import org.apache.sanselan.Sanselan;
 import org.apache.sanselan.common.IImageMetadata;
 import org.apache.sanselan.formats.jpeg.JpegImageMetadata;
 import org.apache.sanselan.formats.tiff.TiffImageMetadata.GPSInfo;
-
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.lang.reflect.Type;
+import org.modelmapper.TypeToken;
+import org.springframework.http.MediaType;
+import com.example.pictgram.bean.TopicCsv;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 
 //import org.thymeleaf.context.Context;
 //import com.example.pictgram.service.SendMailService;
@@ -324,5 +330,17 @@ public class TopicsController {
 			log.warn(e.getMessage(), e);
 		}
 	}
+	@RequestMapping(value = "/topics/topic.csv", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE
+            + "; charset=UTF-8; Content-Disposition: attachment")
+    @ResponseBody
+    public Object downloadCsv() throws IOException {
+        Iterable<Topic> topics = repository.findAll();
+        Type listType = new TypeToken<List<TopicCsv>>() {
+        }.getType();
+        List<TopicCsv> csv = modelMapper.map(topics, listType);
+        CsvMapper mapper = new CsvMapper();
+        CsvSchema schema = mapper.schemaFor(TopicCsv.class).withHeader();
 
+        return mapper.writer(schema).writeValueAsString(csv);
+    }
 }
