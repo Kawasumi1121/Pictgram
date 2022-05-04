1c1c3c3164e0e5df8b6c59d478eca538e01f7c8b
kawasumi <wqgxy387@yahoo.co.jp>
2022-05-05 04:42:45 +0900

    SendMailService-2 #28

--- src/main/java/com/example/pictgram/service/SendMailService.java
+++ src/main/java/com/example/pictgram/service/SendMailService.java
@@ -10,12 +10,16 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
-
+import org.springframework.beans.factory.annotation.Value;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 @Service
 public class SendMailService {
     @Autowired
     private JavaMailSender javaMailSender;
-
+	@Value("${SPRING_MAIL_USERNAME}")
+	private String springMailUsername;
     public void sendMail(Context context) {
 
         javaMailSender.send(new MimeMessagePreparator() {
@@ -23,13 +27,29 @@ public class SendMailService {
             @Override
             public void prepare(MimeMessage mimeMessage) throws Exception {
                 MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, StandardCharsets.UTF_8.name());
-                helper.setFrom("[Gmail の E メールアドレス]");
-                helper.setTo("[Gmail の E メールアドレス]");
-                helper.setSubject("タイトル");
-                helper.setText("本文");
+				helper.setFrom(springMailUsername);
+				helper.setTo(springMailUsername);
+				helper.setSubject((String) context.getVariable("title"));
+				helper.setText(getMailBody("email", context), true);
             }
         });
 
     }
+
+	private String getMailBody(String templateName, Context context) {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(mailTemplateResolver());
+		return templateEngine.process(templateName, context);
+	}
+
+	private ClassLoaderTemplateResolver mailTemplateResolver() {
+		ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		templateResolver.setPrefix("mailtemplates/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setCharacterEncoding("UTF-8");
+		templateResolver.setCacheable(true);
+		return templateResolver;
+	}
 
 }
